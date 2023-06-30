a69217dc9d04f6af8d3b922d9873b5f407857412
benox <benbudnevich@gmail.com>
2023-06-30 10:15:06 -0400

    feat: add connect quickstarts for javascript and react

---
+++ apps/react-connect/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    JS_APP_PORTABL_ACCOUNT_ID: process.env.JS_APP_PORTABL_ACCOUNT_ID,
+    JS_APP_PORTABL_CONNECT_DOMAIN: process.env.JS_APP_PORTABL_CONNECT_DOMAIN,
+    JS_APP_PORTABL_WALLET_DOMAIN: process.env.JS_APP_PORTABL_WALLET_DOMAIN,
+  },
+};
+
+module.exports = nextConfig;
