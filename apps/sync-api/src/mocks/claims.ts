f17389c5206c8666c7fc94359a7263e39630118d
Alexander Yenkalov <34243073+AlexYenkalov@users.noreply.github.com>
2023-07-28 22:30:18 +0400

    fix(sync): mock user data (#21)

--- apps/sync-api/src/mocks/claims.ts
+++ apps/sync-api/src/mocks/claims.ts
@@ -1,21 +1,19 @@
 const MOCKED_CLAIMS = {
   type: 'FinancialCredential', // its a type of the given node, but not a datapoint itself
-  emailAddress: 'liz.lemon@30rock.com',
-  phoneNumber: '+1',
   firstName: 'Liz',
   lastName: 'Lemon',
   birthDate: '1970-11-27',
-  birthPlace: 'PENNSYLVANIA, USA',
-  nationality: 'UNITED STATES OF AMERICA',
+  nationality: 'US',
   socialSecurityNumber: '123-45-6789',
-  sex: 'female',
-  registrationAddressDetails: {
-    type: 'RegistrationAddressDetails', // its a type of the given node, but not a datapoint itself
-    country: 'USA',
+  emailAddress: 'liz.lemon@30rock.com',
+  phoneNumber: '+1-212-456-7890',
+  residentialAddress: {
+    type: 'ResidentialAddress', // its a type of the given node, but not a datapoint itself
+    streetAddress: '168 Riverside Drive, APT 2F',
+    locality: 'New York',
     region: 'New York',
     postalCode: '10024',
-    locality: 'New York',
-    streetAddress: '168 Riverside Drive, APT 2F',
+    country: 'USA',
   },
 };
 
