95806d437b0c9d50e23960f8e07462c61b5b4cc0
benox <benbudnevich@gmail.com>
2024-03-11 11:06:52 -0400

    feat: update connect to use latest changes which allow for next ssr

--- apps/portabl-next/src/components/dashboard/_components/additional-verification.tsx
+++ apps/portabl-next/src/components/dashboard/_components/additional-verification.tsx
@@ -4,7 +4,6 @@ import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/comp
 import { PORTABL_VERIFY_ISSUE_PROJECT_ID } from '@/lib/constants';
 import { useConnect } from '@portabl/react-connect-with-portabl';
 import { useState } from 'react';
-import { useSearchParams } from 'next/navigation';
 
 export default function additionalVerification() {
   const { authorizeWithRedirect, isLoading, isAuthorized, vpToken, user } = useConnect();
