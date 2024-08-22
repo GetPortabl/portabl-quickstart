58c97f2f158128b09fcba879891153a7007f2841
benox <benbudnevich@gmail.com>
2024-08-22 11:16:22 -0400

    chore: remove cache logic for access token
    we can choose to add some better caching here that checks expiration of
    jwt if we find it helpful

--- apps/portabl-next/src/lib/api/get-portabl-client-token.ts
+++ apps/portabl-next/src/lib/api/get-portabl-client-token.ts
@@ -1,22 +1,18 @@
 import { BASE_URL, PORTABL_CLIENT_ID, PORTABL_CLIENT_SECRET } from '../constants';
 
-let accessToken: string;
 export default async function getPortablClientToken() {
-  if (!accessToken) {
-    const tokenResponse = await fetch(`${BASE_URL}/auth/token`, {
-      method: 'POST',
-      body: JSON.stringify({
-        clientId: PORTABL_CLIENT_ID,
-        clientSecret: PORTABL_CLIENT_SECRET,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+  const tokenResponse = await fetch(`${BASE_URL}/auth/token`, {
+    method: 'POST',
+    body: JSON.stringify({
+      clientId: PORTABL_CLIENT_ID,
+      clientSecret: PORTABL_CLIENT_SECRET,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-    const { accessToken: _accessToken } = await tokenResponse.json();
-    accessToken = _accessToken;
-  }
+  const { accessToken } = await tokenResponse.json();
 
   return accessToken;
 }
